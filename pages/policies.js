f6f344b22db1bdfa99b5c44ab034aa89cbf21b88
Maho <maho@vathes.com>
2021-07-03 04:54:53 -0500

    add styling to policies page

--- pages/policies.js
+++ pages/policies.js
@@ -6,9 +6,8 @@ export default function Policies() {
       <div className="policies-section">
         <h3 className="section-title">Policies</h3>
         <div>
-          <h4>Policy</h4>
           <p>Please click on the policy to download. 
-          <ul>
+          <ul className="policy-list">
             <li><a href="/static/documents/FCOI POLICY - DataJoint Financial Conflict of Interest Policy - June 2021.pdf" download>DataJoint Financial Conflict of Interest Policy</a></li> 
           </ul>
           </p>
@@ -16,16 +15,32 @@ export default function Policies() {
       </div>
       <style jsx>{`
           .policies-section {
-
+            position: relative;
+            padding: 120px 0 10%;
+            width: 80%;
+            margin: 0 auto;
           }
 
           .section-title {
-            
+            font-weight: 600;
+            font-style: italic;
+            color: #0F3595;
+            margin: 0;
           }
-          
 
-          @media (max-width: 480px) {
-          
+          .policy-list a {
+            text-decoration: none;
+            color: #3293F1;
+          }
+
+          .policy-list a:hover {
+            color: #1CA9F9;
+          }
+
+          @media (max-width: 940px) {
+            .policies-section {
+              width: 90%;
+            }
           }
       }
       `}</style>
